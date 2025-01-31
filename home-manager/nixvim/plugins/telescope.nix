063187f4224a36281290ee28e8822a37710ed7a3
aw-andre <aw.andregerard@gmail.com>
2025-01-30 19:39:22 -0500

    feat: change telescope keymaps from strings to lua

--- home-manager/nixvim/plugins/telescope.nix
+++ home-manager/nixvim/plugins/telescope.nix
@@ -3,96 +3,90 @@
     enable = true;
     extensions = {
       fzf-native.enable = true;
+      live-grep-args.enable = true;
       ui-select.enable = true;
       undo.enable = true;
-      live-grep-args.enable = true;
     };
   };
   keymaps = [
             {
               mode = "n";
               key = "<leader>fh";
-              action = "<Cmd>Telescope help_tags<CR>";
+              action.__raw = "require('telescope.builtin').help_tags";
               options = { desc = "[F]ind [H]elp"; };
             }
             {
               mode = "n";
               key = "<leader>fk";
-              action = "<Cmd>Telescope keymaps<CR>";
+              action.__raw = "require('telescope.builtin').keymaps";
               options = { desc = "[F]ind [K]eymaps"; };
             }
             {
               mode = "n";
               key = "<leader>ff";
-              action = "<Cmd>Telescope find_files<CR>";
+              action.__raw = "require('telescope.builtin').find_files";
               options = { desc = "[F]ind [F]iles"; };
             }
 #            {
 #              mode = "n";
 #              key = "<leader>ft";
-#              action = "<Cmd>Telescope telescope.builtin<CR>";
+#              action.__raw = "require('telescope.builtin').require('telescope.builtin')";
 #              options = { desc = "[F]ind in [T]elescope"; };
 #            }
             {
               mode = "n";
               key = "<leader>fw";
-              action = "<Cmd>Telescope grep_string<CR>";
+              action.__raw = "require('telescope.builtin').grep_string";
               options = { desc = "[F]ind current [W]ord"; };
             }
             {
               mode = "n";
               key = "<leader>fg";
-              action = "<Cmd>Telescope live_grep<CR>";
+              action.__raw = "require('telescope.builtin').live_grep";
               options = { desc = "[F]ind by [G]rep"; };
             }
             {
               mode = "n";
               key = "<leader>fd";
-              action = "<Cmd>Telescope diagnostics<CR>";
+              action.__raw = "require('telescope.builtin').diagnostics";
               options = { desc = "[F]ind [D]iagnostics"; };
             }
             {
               mode = "n";
               key = "<leader>f.";
-              action = "<Cmd>Telescope resume<CR>";
+              action.__raw = "require('telescope.builtin').resume";
               options = { desc = "[F]ind [.] Repeat"; };
             }
             {
               mode = "n";
               key = "<leader>fr";
-              action = "<Cmd>Telescope oldfiles<CR>";
+              action.__raw = "require('telescope.builtin').oldfiles";
               options = { desc = "[F]ind [R]ecent Files"; };
             }
             {
               mode = "n";
               key = "<leader>fb";
-              action = "<Cmd>Telescope buffers<CR>";
+              action.__raw = "require('telescope.builtin').buffers";
               options = { desc = "[F]ind [B]uffers"; };
             }
             {
               mode = "n";
-              key = "<leader>fu";
-              action = "<Cmd>Telescope undoCR>";
-              options = { desc = "[F]ind [U]ndotree"; };
+              key = "<leader>f%";
+              action.__raw = "function() require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown { winblend = 10, previewer = false, }) end";
+              options = { desc = "[F]ind [%] in Current Buffer"; };
             }
-#            {
-#              mode = "n";
-#              key = "<leader>f%";
-#              action = "function() <Cmd>Telescope current_buffer_fuzzy_find(require('telescope.themes').get_dropdown { winblend = 10, previewer = false, }) end<CR>";
-#              options = { desc = "[F]ind [%] in Current Buffer"; };
-#            }
-#            {
-#              mode = "n";
-#              key = "<leader>f/";
-#              action = "function() <Cmd>Telescope live_grep { grep_open_files = true, prompt_title = 'Live Grep in Open Files', } end<CR>";
-#              options = { desc = "[F]ind [/] in Open Files"; };
-#            }
-#            {
-#              mode = "n";
-#              key = "<leader>fn";
-#              action = "<Cmd>Telescope find_files ~/flake/home-manager/nixvim/<CR>";
-#              options = { desc = "[F]ind [N]eovim files"; };
-#            }
+            {
+              mode = "n";
+              key = "<leader>f/";
+              action.__raw = "function() require('telescope.builtin').live_grep { grep_open_files = true, prompt_title = 'Live Grep in Open Files', } end";
+              options = { desc = "[F]ind [/] in Open Files"; };
+            }
+            {
+              mode = "n";
+              key = "<leader>fn";
+              action.__raw = "function() require('telescope.builtin').find_files { cwd = '~/flake/home-manager/nixvim/' } end";
+              options = { desc = "[F]ind [N]eovim files"; };
+            }
   ];
   autoCmd = [
     {
