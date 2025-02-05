9563e87e017d22b4a181402c7938c6a67f922bb1
aw-andre <aw.andregerard@gmail.com>
2025-02-04 20:04:10 -0500

    fix: make linters work

--- home-manager/nixvim/plugins/lint.nix
+++ home-manager/nixvim/plugins/lint.nix
@@ -9,12 +9,12 @@
       nix = [ "statix" ];
       python = [ "ruff" ];
     };
-    linters = {
-      shellcheck.command = lib.getExe pkgs.shellcheck;
-      cppcheck.command = lib.getExe pkgs.cppcheck;
-      luacheck.command = lib.getExe pkgs.luajitPackages.luacheck;
-      statix.command = lib.getExe pkgs.statix;
-      ruff.command = lib.getExe pkgs.ruff;
-    };
   };
+  extraPackages = with pkgs; [
+    shellcheck
+    cppcheck
+    luajitPackages.luacheck
+    statix
+    ruff
+  ];
 }
