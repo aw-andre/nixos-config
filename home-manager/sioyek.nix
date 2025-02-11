77478973cc0867202263699d8177f52d2b215161
aw-andre <aw.andregerard@gmail.com>
2025-02-11 00:56:49 -0500

    feat: add Sioyek bindings and configuration

--- home-manager/sioyek.nix
+++ home-manager/sioyek.nix
@@ -1,5 +1,30 @@
 {
   programs.sioyek = {
     enable = true;
+    bindings = {
+      move_down_smooth = "j";
+      move_up_smooth = "k";
+      move_left = "h";
+      move_right = "l";
+      next_page = "<C-f>";
+      previous_page = "<C-b>";
+      screen_down = "<C-d>";
+      screen_up = "<C-u>";
+      prev_state = "<C-o>";
+      next_state = "<C-i>";
+      new_window = "<C-w>s";
+      close_window = "<C-w>q";
+      goto_toc = "t";
+      zoom_in = "<C-=>";
+      fit_to_page_width = "<C-0>";
+      zoom_out = "<C-->";
+      regex_search = "/";
+    };
+    config = {
+      font_size = "16";
+      startup_commands = "toggle_dark_mode";
+      collapsed_toc = "1";
+      super_fast_search = "1";
+    };
   };
 }
