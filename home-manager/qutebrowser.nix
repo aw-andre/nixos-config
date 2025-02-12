992fc2ded4d20a3de8f42fe2d2232dd125b9f843
aw-andre <aw.andregerard@gmail.com>
2025-02-11 20:25:19 -0500

    feat: add Qutebrowser keybinds

--- home-manager/qutebrowser.nix
+++ home-manager/qutebrowser.nix
@@ -2,5 +2,33 @@
   programs.qutebrowser = {
     enable = true;
     settings.colors.webpage.darkmode.enabled = true;
+    keyMappings = {
+      "<Ctrl-[>" = "<Escape>";
+      "<Ctrl-m>" = "<Return>";
+      "<Ctrl-i>" = "<Tab>";
+      "<Ctrl-h>" = "<Backspace>";
+    };
+    keyBindings = {
+      normal = {
+        "<Ctrl-->" = "zoom-out";
+        "<Ctrl-+>" = "zoom-in";
+        "<Ctrl-0>" = "zoom";
+        "gt" = "tab-next";
+        "gT" = "tab-prev";
+        "u" = "back";
+        "<Ctrl-r>" = "forward";
+        "x" = "tab-close";
+        "X" = "tab-close -o";
+        "<Ctrl-1>" = "tab-focus 1";
+        "<Ctrl-2>" = "tab-focus 2";
+        "<Ctrl-3>" = "tab-focus 3";
+        "<Ctrl-4>" = "tab-focus 4";
+        "<Ctrl-5>" = "tab-focus 5";
+        "<Ctrl-6>" = "tab-focus 6";
+        "<Ctrl-7>" = "tab-focus 7";
+        "<Ctrl-8>" = "tab-focus 8";
+        "<Ctrl-9>" = "tab-focus 9";
+      };
+    };
   };
 }
