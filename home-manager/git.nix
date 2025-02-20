ccddb5d7a33150ae33cbd22aad4824d8b397b794
aw-andre <aw.andregerard@gmail.com>
2025-02-20 12:30:30 -0500

    feat!: make Git aliases more idiomatic

--- home-manager/git.nix
+++ home-manager/git.nix
@@ -4,10 +4,12 @@
     userName = "aw-andre";
     userEmail = "aw.andregerard@gmail.com";
     aliases = {
-      basei = "rebase -i HEAD~10";
+      rebasei = "rebase -i HEAD~10";
       loga = "log --oneline --graph --all";
-      sets = "reset --soft";
-      seth = "reset --hard";
+      resets = "reset --soft";
+      reseth = "reset --hard";
+      pushm = "push origin master";
+      pushmf = "push --force origin master";
     };
     extraConfig.core = {
       editor = "nvim";
