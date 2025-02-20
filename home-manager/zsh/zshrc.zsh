183f3cd8227652ac307bc2be62a872d4eed1c310
aw-andre <aw.andregerard@gmail.com>
2025-02-20 12:45:19 -0500

    feat: hide pushd, popd stdout on rebuild

--- home-manager/zsh/zshrc.zsh
+++ home-manager/zsh/zshrc.zsh
@@ -3,12 +3,12 @@ export MANPAGER="nvim +Man!"
 
 rebuild () (
   set -eK
-  pushd ~/files/nixos-config/
+  pushd ~/files/nixos-config/ &> /dev/null
   git add .
   sudo nixos-rebuild switch --flake .#andreaw --show-trace
   gen=$(nixos-rebuild list-generations | grep current)
   git commit -am "$gen"
-  popd
+  popd &> /dev/null
 )
 
 cd ~/files
