1c954ec104553af19e9baf4a3369ea962c9bc548
aw-andre <aw.andregerard@gmail.com>
2025-02-19 22:28:11 -0500

    feat: implement rebuilding function as subshell

--- home-manager/zsh/zshrc.zsh
+++ home-manager/zsh/zshrc.zsh
@@ -1,5 +1,6 @@
-rebuild () {
+rebuild () (
+  set -eK
   sudo nixos-rebuild switch --flake .#andreaw --show-trace
   gen=$(nixos-rebuild list-generations | grep current)
   git commit -am "$gen"
-}
+)
