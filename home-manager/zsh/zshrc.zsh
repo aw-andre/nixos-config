f958e329817e123e8133bb8257dbaf64d415c94a
aw-andre <aw.andregerard@gmail.com>
2025-02-20 00:17:11 -0500

    feat: make rebuild function more robust

--- home-manager/zsh/zshrc.zsh
+++ home-manager/zsh/zshrc.zsh
@@ -3,7 +3,10 @@ export MANPAGER="nvim +Man!"
 
 rebuild () (
   set -eK
+  pushd ~/files/nixos-config/
+  git add .
   sudo nixos-rebuild switch --flake .#andreaw --show-trace
   gen=$(nixos-rebuild list-generations | grep current)
   git commit -am "$gen"
+  popd
 )
