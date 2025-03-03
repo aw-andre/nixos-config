aaae13c3854a052a90a19f4b8154fd3e8d905cd2
aw-andre <aw.andregerard@gmail.com>
2025-03-03 01:42:04 -0500

    feat: set Kitty keybindings

--- home-manager/kitty.nix
+++ home-manager/kitty.nix
@@ -3,10 +3,23 @@
     enable = true;
     font.name = "JetBrains Mono";
     font.size = 14;
+    keybindings = {
+      "ctrl+c" = "copy_or_interrupt";
+      "ctrl+v" = "paste_from_clipboard";
+      "ctrl+shift+/" = "show_scrollback";
+      "ctrl+shift+." = "show_last_command_output";
+      "ctrl+shift+b" = "scroll_page_up";
+      "ctrl+shift+f" = "scroll_page_down";
+      "ctrl+shift+^" = "scroll_home";
+      "ctrl+shift+$" = "scroll_end";
+      "ctrl+shift+u" = "scroll_to_prompt -1";
+      "ctrl+shift+d" = "scroll_to_prompt 1";
+      "ctrl+shift+0" = "change_font_size current 14.0";
+      "ctrl+shift+minus" = "change_font_size current -1.0";
+      "ctrl+shift+equal" = "change_font_size current +1.0";
+    };
     themeFile = "VSCode_Dark";
-    settings.scrollback_pager = ''
-      nvim -c "silent! w! /tmp/kitty_scrollback_buffer | exec 'te cat /tmp/kitty_scrollback_buffer -' | exec 'norm G' | bn | bd!"
-    '';
+    settings.scrollback_pager = "vimp";
     extraConfig = ''
       enable_audio_bell no
       modify_font cell_height 150%
