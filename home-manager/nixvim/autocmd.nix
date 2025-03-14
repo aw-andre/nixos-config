c7652fc5e3365ceed41684dc6b4725f35312d708
aw-andre <aw.andregerard@gmail.com>
2025-03-13 23:33:39 -0400

    fix: update rainbow delimiters on write

--- home-manager/nixvim/autocmd.nix
+++ home-manager/nixvim/autocmd.nix
@@ -13,7 +13,7 @@
     }
     {
       event = "BufWritePre";
-      callback.__raw = "function() vim.cmd('normal =<CMD>e<CR>') end";
+      callback.__raw = "function() vim.schedule(function () vim.lsp.buf.format() vim.cmd.edit() end) end";
       group = "write";
       desc = "Format when writing text";
     }
