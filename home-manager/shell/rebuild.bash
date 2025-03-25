95d89a1df3fdf9c457da94f1179588c7ded8486e
aw-andre <aw.andregerard@gmail.com>
2025-03-25 13:18:40 -0400

    build: make rebuild script resolve submodule conflicts

--- home-manager/shell/rebuild.bash
+++ home-manager/shell/rebuild.bash
@@ -1,6 +1,7 @@
 set -e
-pushd ~/files/nixos-config/ &>/dev/null
-git submodule foreach git pull origin master
+cd ~/files/nixos-config/
+cd home-manager/nixvim && git reset --hard origin/master
+cd -
 git add .
 sudo nixos-rebuild switch --flake '.?submodules=1#andreaw' --show-trace
 gen=$(nixos-rebuild list-generations | grep current)
