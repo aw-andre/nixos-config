6a228ed5f20108fa20b60d5a092537daf7cdbda6
aw-andre <aw.andregerard@gmail.com>
2025-04-09 14:29:42 -0400

    feat: make rebuild script more robust

--- home-manager/shell/rebuild.bash
+++ home-manager/shell/rebuild.bash
@@ -1,9 +1,8 @@
 set -e
 cd ~/files/nixos-config/
-cd home-manager/nixvim && git reset --hard origin/master
+cd home-manager/nixvim && git fetch origin && git reset --hard origin/master
 cd -
 git add .
 sudo nixos-rebuild switch --flake '.?submodules=1#andreaw' --show-trace
 gen=$(nixos-rebuild list-generations | grep current)
 git commit -am "$gen"
-popd &>/dev/null
