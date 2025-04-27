b3cf5b1a77ff7e20fc9614ed839b917916393e23
aw-andre <aw.andregerard@gmail.com>
2025-04-27 15:11:20 -0400

    fix: make CTRL-T shortcut in fzf respect gitignore

--- home-manager/fzf.nix
+++ home-manager/fzf.nix
@@ -1,9 +1,13 @@
 {
-  programs.fzf = {
-    enable = true;
-    enableZshIntegration = true;
-    defaultCommand = "fd --type f --hidden --exclude .git";
-    defaultOptions =
-      [ "--preview 'bat --style=numbers --color=always --line-range=:500 {}'" ];
+  programs = {
+    fzf = {
+      enable = true;
+      enableZshIntegration = true;
+      defaultCommand = "fd --type f --hidden --exclude .git";
+      defaultOptions = [
+        "--preview 'bat --style=numbers --color=always --line-range=:500 {}'"
+      ];
+    };
+    zsh.initExtra = "FZF_CTRL_T_COMMAND='fd --type f --hidden --exclude .git'";
   };
 }
