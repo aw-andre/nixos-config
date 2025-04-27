23d0cb5478b99c05316e98f9a6df47fdd58c2532
aw-andre <aw.andregerard@gmail.com>
2025-04-27 15:36:59 -0400

    feat: make glob fuzzy completion respect gitignore

--- home-manager/fzf.nix
+++ home-manager/fzf.nix
@@ -8,6 +8,16 @@
         "--preview 'bat --style=numbers --color=always --line-range=:500 {}'"
       ];
     };
-    zsh.initExtra = "FZF_CTRL_T_COMMAND='fd --type f --hidden --exclude .git'";
+    zsh.initExtra = ''
+      FZF_CTRL_T_COMMAND='fd --type f --hidden --exclude .git'
+      FZF_ALT_C_COMMAND='fd --type d --hidden --exclude .git'
+
+      _fzf_compgen_path() {
+        fd --hidden --follow --exclude ".git" . "$1"
+      }
+      _fzf_compgen_dir() {
+        fd --type d --hidden --follow --exclude ".git" . "$1"
+      }
+    '';
   };
 }
