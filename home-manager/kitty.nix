85b746a73a643179950bdc9b77483b426ab992fc
aw-andre <aw.andregerard@gmail.com>
2025-06-26 14:15:45 -0400

    feat!: remove window padding

--- home-manager/kitty.nix
+++ home-manager/kitty.nix
@@ -32,7 +32,6 @@
       italic_font Comic Code Italic
       bold_italic_font Comic Code Bold Italic
       font_size 14.0
-      window_padding_width 10 5
       tab_bar_edge top
       tab_bar_style powerline
       enable_audio_bell no
