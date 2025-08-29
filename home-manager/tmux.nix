a557821d025a7c35c5970b4296e5ee44bfa443f1
aw-andre <aw.andregerard@gmail.com>
2025-08-29 16:10:21 -0400

    fix: add true color support for tmux

--- home-manager/tmux.nix
+++ home-manager/tmux.nix
@@ -4,6 +4,10 @@
     keyMode = "vi";
     prefix = "Delete";
     mouse = true;
+    terminal = "tmux-256color";
     plugins = with pkgs.tmuxPlugins; [ yank ];
+    extraConfig = ''
+      set -ga terminal-overrides ',xterm-256color:Tc'
+    '';
   };
 }
