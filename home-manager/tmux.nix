9d891f1bc7e44f796661744889dd1b870be7d08f
aw-andre <aw.andregerard@gmail.com>
2025-08-29 17:55:05 -0400

    feat: configure tmux

--- home-manager/tmux.nix
+++ home-manager/tmux.nix
@@ -4,10 +4,17 @@
     keyMode = "vi";
     prefix = "Delete";
     mouse = true;
+    escapeTime = 0;
     terminal = "tmux-256color";
     plugins = with pkgs.tmuxPlugins; [ yank ];
     extraConfig = ''
       set -ga terminal-overrides ',xterm-256color:Tc'
+      set -g focus-events on
+      set -g status-style bg=default
+      set -g status-position top
+      set -g status-left ""
+      set -g status-right ""
+      set -g status-justify centre
     '';
   };
 }
