1de6aa62feb577c43e739fef2ce851bc696301d0
aw-andre <aw.andregerard@gmail.com>
2025-09-16 15:56:25 -0400

    feat: add window switching to run command

--- home-manager/shell/run.bash
+++ home-manager/shell/run.bash
@@ -1,4 +1,26 @@
 TUI=("vim" "nvim" "gemini" "lldb" "gdb")
+ARGS=()
+
+while [[ $# -gt 0 ]]; do
+	case "$1" in
+	-w)
+		if [[ ! "$2" =~ ^[0-9]$ ]]; then
+			echo "Must pass a digit!"
+			exit 1
+		fi
+
+		WINDOW="$2"
+		shift 2
+		;;
+	*)
+		ARGS+=("$1")
+		shift
+		;;
+	esac
+done
+
+set -- "${ARGS[@]}"
+
 BIN="$1"
 shift
 
@@ -9,4 +31,13 @@ else
 fi
 
 $CMD &>/dev/null &
+PID=$!
 disown
+
+if [[ -v WINDOW ]]; then
+	until hyprctl clients -j | jq -e ".[] | select(.pid == $PID)" &>/dev/null; do
+		sleep 0.1
+	done
+
+	hyprctl dispatch movetoworkspacesilent "$WINDOW",pid:"$PID" &>/dev/null
+fi
