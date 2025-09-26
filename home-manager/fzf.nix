bdc86214ac3ac9bfc73154aac06b7a9fa2004f7f
aw-andre <aw.andregerard@gmail.com>
2025-09-26 09:52:00 -0400

    feat: improve fzf

--- home-manager/fzf.nix
+++ home-manager/fzf.nix
@@ -3,21 +3,23 @@
     fzf = {
       enable = true;
       enableZshIntegration = true;
-      defaultCommand = "fd --type f --hidden --exclude .git";
+      defaultCommand = "fd --type f --hidden --exclude .git -a";
       defaultOptions = [
         "--preview 'bat --style=numbers --color=always --line-range=:500 {}'"
       ];
     };
     zsh.initContent = ''
-      FZF_CTRL_T_COMMAND='fd --type f --hidden --exclude .git'
-      FZF_ALT_C_COMMAND='fd --type d --hidden --exclude .git'
+      FZF_CTRL_T_COMMAND='fd --type f --hidden --exclude .git -a .'
+      FZF_ALT_C_COMMAND='fd --type d --hidden --exclude .git -a .'
 
       _fzf_compgen_path() {
-        fd --type f --hidden --exclude .git
+        fd --type f --hidden --exclude .git -a . "$1"
       }
       _fzf_compgen_dir() {
-        fd --type d --hidden --exclude .git
+        fd --type d --hidden --exclude .git -a . "$1"
       }
+
+
     '';
   };
 }
--- home-manager/kitty.nix
+++ home-manager/kitty.nix
@@ -40,7 +40,7 @@
       tab_bar_style powerline
       enable_audio_bell no
       modify_font cell_height 150%
-      background_opacity 0.9
+      background_opacity 0.8
       cursor_shape_unfocused unchanged
       cursor_trail 1
       cursor_trail_decay 0.1 0.3
