4b1534e2d8638e2d96371b9a693ea7f498ae95a2
aw-andre <aw.andregerard@gmail.com>
2025-10-05 18:50:51 -0400

    feat: add nh

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -125,6 +125,12 @@
       enable = true;
       hidden = true;
     };
+    nh = {
+      enable = true;
+      clean.enable = true;
+      clean.extraArgs = "--keep-since 7d";
+      flake = "/home/andreaw/files/configs/laptop-config/";
+    };
     home-manager.enable = true;
     google-chrome.enable = true;
     nix-index.enable = true;
