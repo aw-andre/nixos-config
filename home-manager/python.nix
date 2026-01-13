9848054c7ed13882eb7c17e9e830682cd84fcd27
aw-andre <aw.andregerard@gmail.com>
2026-01-13 14:25:44 -0500

    feat: make ipython use the pager

--- home-manager/python.nix
+++ home-manager/python.nix
@@ -8,6 +8,7 @@
         c.TerminalInteractiveShell.confirm_exit = False
         c.TerminalInteractiveShell.autoindent = True
         c.TerminalInteractiveShell.highlight_matching_brackets = True
+        c.TerminalInteractiveShell.display_page = True
       '';
       target = ".ipython/profile_default/ipython_config.py";
     };
