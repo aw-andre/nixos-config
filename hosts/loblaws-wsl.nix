fbc08ef11aa270230da147ff5632a4157d937e44
aw-andre <aw.andregerard@gmail.com>
2026-02-11 20:03:38 -0500

    fix: use proper uid in /run/user, bindmount wslg

--- hosts/loblaws-wsl.nix
+++ hosts/loblaws-wsl.nix
@@ -5,14 +5,28 @@
   wsl = {
     enable = true;
     defaultUser = "andreaw";
-    docker-desktop.enable = true;
-    useWindowsDriver = true;
-    wslConf.automount.options = "uid=1000,gid=100";
+  };
+
+  systemd = {
+    services.fix-wsl-runtime = {
+      serviceConfig = {
+        Type = "oneshot";
+        RemainAfterExit = true;
+      };
+      script = ''
+        chown 1000:1000 /run/user/1000
+        chmod 700 /run/user/1000
+        mkdir -p /run/user/1000/wslg
+        mount --bind /mnt/wslg/runtime-dir /run/user/1000/wslg
+        ln -sf /mnt/wslg/runtime-dir/wayland-0 /run/user/1000/wayland-0
+      '';
+      wantedBy = [ "multi-user.target" ];
+    };
+    paths.fix-wsl-runtime = {
+      wantedBy = [ "multi-user.target" ];
+      pathConfig.PathExists = "/run/user/1000";
+    };
   };
 
   security.pki.certificateFiles = [ ./loblaws-cert.crt ];
-
-  # users.users.andreaw.openssh.authorizedKeys.keys = [
-  #   "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBRjAP//SG64jLi0HZ2Pep2Zu0NtmYYZGHLDQ2w1sg+u andreaw@mbp"
-  # ];
 }
