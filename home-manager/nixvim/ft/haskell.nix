ce4f71fc5ade6fc3d566c1847dbde5497ba62230
aw-andre <aw.andregerard@gmail.com>
2026-03-11 20:59:53 -0400

    feat: add haskell scope highlights

--- home-manager/nixvim/ft/haskell.nix
+++ home-manager/nixvim/ft/haskell.nix
@@ -3,4 +3,31 @@
     haskell-tools.enable = true;
     haskell-scope-highlighting.enable = true;
   };
+
+  highlightOverride = {
+    HaskellVariableDeclarationWithinScope.link = "GruvboxGreenBold";
+    HaskellVariableNotDeclaredWithinFile.link = "GruvboxPurpleBold";
+    HaskellVariableDeclaredWithinFile.link = "GruvboxRedBold";
+    HaskellVariableDeclaredWithinScope.link = "GruvboxBlueBold";
+    HaskellVariableDeclaredWithinParent1.link = "GruvboxAquaBold";
+    HaskellVariableDeclaredWithinParent2.link = "GruvboxAquaBold";
+    HaskellVariableDeclaredWithinParent3.link = "GruvboxAquaBold";
+    HaskellVariableDeclaredWithinParent4.link = "GruvboxAquaBold";
+    HaskellVariableDeclaredWithinParent5.link = "GruvboxAquaBold";
+    HaskellVariableDeclaredWithinParent6.link = "GruvboxAquaBold";
+    HaskellVariableDeclaredWithinParent7.link = "GruvboxAquaBold";
+    HaskellVariableDeclaredWithinParent8.link = "GruvboxAquaBold";
+    HaskellVariableDeclaredWithinParent9.link = "GruvboxAquaBold";
+    HaskellVariableDeclaredWithinParent10.link = "GruvboxAquaBold";
+    HaskellVariableDeclaredWithinParent11.link = "GruvboxAquaBold";
+    HaskellVariableDeclaredWithinParent12.link = "GruvboxAquaBold";
+    HaskellVariableDeclaredWithinParent13.link = "GruvboxAquaBold";
+    HaskellVariableDeclaredWithinParent14.link = "GruvboxAquaBold";
+    HaskellVariableDeclaredWithinParent15.link = "GruvboxAquaBold";
+    HaskellVariableDeclaredWithinParent16.link = "GruvboxAquaBold";
+    HaskellVariableDeclaredWithinParent17.link = "GruvboxAquaBold";
+    HaskellVariableDeclaredWithinParent18.link = "GruvboxAquaBold";
+    HaskellVariableDeclaredWithinParent19.link = "GruvboxAquaBold";
+    HaskellVariableDeclaredWithinParent20.link = "GruvboxAquaBold";
+  };
 }
